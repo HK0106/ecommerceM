dd38c097ce776705f3bf07324d5719463062d977
HK0106 <Hiro27060106@gmail.com>
2021-01-12 08:05:14 +0700

    first

---
+++ src/test/java/maidx/spring/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package maidx.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
